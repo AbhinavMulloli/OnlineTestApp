545244270b3b83a9da7b01f2519a2f41a73db833
AbhinavMulloli <153629886+AbhinavMulloli@users.noreply.github.com>
2024-02-21 06:41:59 +0000

    added instructions,auto submiting when user switches tabs,warning message after 2 mins,route protection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Route, Routes } from "react-router-dom";
+import {Route, Routes,Navigate } from "react-router-dom";
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
 import Dashboard from './Components/Dashboard/Dashboard';
@@ -14,10 +14,18 @@ export default function App() {
   <Routes>
     <Route path="/" element={ <Login />}></Route>
      <Route path="/register" element={ <Register />}></Route>
-    <Route path="/dashboard" element={ <Dashboard />}></Route>
+    <Route path="/dashboard" element={<ProtectedRoutes><Dashboard/></ProtectedRoutes>}></Route>
     <Route path="/questions/:id" element={ <Question />}></Route>
     <Route path="/forgetpass" element={ <ForgetPass />}></Route>
     <Route path="/updatepass/:id" element={ <UpdatePass />}></Route>
   </Routes>
   )
+}
+export function ProtectedRoutes(props){
+  if(localStorage.getItem('email')){
+    return props.children
+  }
+  else{
+    return <Navigate to="/"/>
+  }
 }
